c9d131215fc3bb2c5154e1e87006b6c1458ea2d5
exepirit <exepir1t@protonmail.com>
2025-08-05 18:47:13 +0700

    u-boot: match offsets in env.txt

--- lib/envimage/parsing.py
+++ lib/envimage/parsing.py
@@ -4,24 +4,33 @@ def parse_partition(partition):
         number, unit = size[:-1], size[-1]
         return int(float(number)*units[unit])
 
-    name = partition.split("(")[1].rstrip(")")
-    size = parse_size(partition.split("(")[0].split("@")[0])
-    return (name, size)
+    size_offset, name = tuple(partition.rstrip(")").split("("))
+    size_offset = size_offset.split("@")
+    size = parse_size(size_offset[0])
+    if len(size_offset) > 1:
+        offset = parse_size(size_offset[1])
+    else:
+        offset = -1
+    return (name, size, offset)
 
 
 def iterate_partitions(env_str):
     offset = 0
     for part in env_str.split(","):
-        name, size = parse_partition(part)
-        yield (name, size, offset)
-        offset += size
+        name, size, defined_offset = parse_partition(part)
+        if defined_offset >= 0:
+            yield (name, size, defined_offset)
+            offset = defined_offset + size
+        else:
+            yield (name, size, offset)
+            offset += size
 
 
 def get_partition(env_str, partition_name):
     for i, partition in enumerate(iterate_partitions(env_str)):
         name, size, offset = partition
         if name == partition_name:
-            return (i+1, hex(size), hex(offset))
+            return (i+1, size, offset)
     raise Exception(f"Partition {partition_name} not found in environment string")
 
 
@@ -32,9 +41,9 @@ def get_partition_index(env_str, partition_name):
 
 def get_partition_size(env_str, partition_name):
     _, size, _ = get_partition(env_str, partition_name)
-    return size
+    return hex(size)
 
 
 def get_partition_offset(env_str, partition_name):
     _, _, offset = get_partition(env_str, partition_name)
-    return offset
+    return hex(offset)
